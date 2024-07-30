fbb47200ce184533ad7f249165a5d44064e76918
Marah Ashayer <marahashayer317@gmail.com>
2024-07-30 21:24:35 +0300

    Modify Favorites Storage

--- src/context/FavoritesContext.jsx
+++ src/context/FavoritesContext.jsx
@@ -1,28 +1,29 @@
-// src/context/FavoritesContext.jsx
 import React, { createContext, useState, useEffect } from 'react';
 
 export const FavoritesContext = createContext();
 
 export const FavoritesProvider = ({ children }) => {
-  const [favorites, setFavorites] = useState([]);
-
-  useEffect(() => {
-    const storedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
-    setFavorites(storedFavorites);
-  }, []);
-
+  const [favorites, setFavorites] = useState(() => {
+    const savedFavorites = localStorage.getItem('favorites');
+    return savedFavorites ? JSON.parse(savedFavorites) : [];
+  });
   useEffect(() => {
     localStorage.setItem('favorites', JSON.stringify(favorites));
   }, [favorites]);
 
   const addToFavorites = (course) => {
-    setFavorites((prevFavorites) => [...prevFavorites, course]);
+    setFavorites((prevFavorites) => {
+      const updatedFavorites = [...prevFavorites, course];
+      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+      return updatedFavorites;
+    });
   };
-
   const removeFromFavorites = (courseId) => {
-    setFavorites((prevFavorites) =>
-      prevFavorites.filter((course) => course.id !== courseId)
-    );
+    setFavorites((prevFavorites) => {
+      const updatedFavorites = prevFavorites.filter(course => course.id !== courseId);
+      localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+      return updatedFavorites;
+    });
   };
 
   const isFavorite = (courseId) => {
